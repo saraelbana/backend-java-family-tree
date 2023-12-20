fbd19de340e7720b508e1fe2d25f7e131e0ab9e6
saraelbana <matika20@gmail.com>
2023-12-20 13:38:36 +0200

    Created the Person and Pet classes with the basic attributes, setters and getters that was asked for in the readme file

---
+++ src/main/Person.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String sex;
+    private Person mother = new Person();
+    private Person father = new Person();
+    private List<Person> siblings = new ArrayList<Person>();
+    private List<Person> children = new ArrayList<Person>();
+    private List<Pet> Pets = new ArrayList<Pet>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public void setSiblings(List<Person> siblings) {
+        this.siblings = siblings;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public void setPets(List<Pet> pets) {
+        Pets = pets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public List<Person> getSiblings() {
+        return siblings;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public List<Pet> getPets() {
+        return Pets;
+    }
+    public List<Person> getGrandChildren(){
+        List<Person> grandChildren = new ArrayList<Person>();
+        for(int i =0; i<children.size();i++)
+        {
+            for(int j = 0; j<children.get(i).children.size();j++){
+                getGrandChildren().add(children.get(i).children.get(j));
+            }
+        }
+        return grandChildren;
+    }
+}
---
+++ src/main/Pet.java
@@ -0,0 +1,43 @@
+public class Pet {
+    /*name
+age
+species
+owner (Person)
+*/
+    private String name;
+    private int age;
+    private String species;
+    private Person owner = new Person();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+}
